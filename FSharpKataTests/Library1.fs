a057a414008b0d2dc65d752a1e80b4a8d44c7817
unknown <jyotima@jyotima-l.fareast.corp.microsoft.com>
2014-07-13 02:19:46 +0530

    formatted using Fantomas

--- FSharpKataTests/Library1.fs
+++ FSharpKataTests/Library1.fs
@@ -1,21 +1,18 @@
 ﻿// Learn more about f# at http://fsharp.net
-
 namespace FSharpKataTests
 
 exception ThingException of string
 
 type Class1() = 
     let someFunction = 
-        if (4 + 5) <= 5 then
-            1
-        else
-            3
+        if (4 + 5) <= 5 then 1
+        else 3
 
-type IExampleInterface =
-   abstract member Print : unit -> unit
+type IExampleInterface = 
+    abstract Print : unit -> unit
 
 type Class2() = 
     let main argv = 
-        let x = List.sum [1;2;3]
+        let x = List.sum [ 1; 2; 3 ]
         printfn "%d" x
         0
