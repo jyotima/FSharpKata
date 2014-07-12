a366c1850515ce9b73089b52767ae656a528a771
unknown <jyotima@jyotima-l.fareast.corp.microsoft.com>
2014-07-13 01:09:53 +0530

    FSharpLint failing

--- FSharpKataTests/Library1.fs
+++ FSharpKataTests/Library1.fs
@@ -1,4 +1,21 @@
-﻿namespace FSharpKataTests
+﻿// Learn more about f# at http://fsharp.net
+
+namespace FSharpKataTests
+
+exception Thing of string
 
 type Class1() = 
-    member this.X = "F#"
+    let Abc = 
+        if (4 + 5) > 5 |> not then
+            1
+        else
+            3
+
+type ExampleInterface =
+   abstract member print : unit -> unit
+
+type Class2() = 
+    let main argv = 
+        let x = List.fold (fun x y -> x + y) 0 [1;2;3]
+        printfn "%d" x
+        0
