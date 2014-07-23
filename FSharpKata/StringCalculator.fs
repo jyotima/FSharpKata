1bb829ad6817cddc5f6f0266d2c9678316b24d6b
jyotima <jyotima@live.com>
2014-07-23 23:47:58 +0530

    Kata StringCalculator again with None example used instead of throwing

--- FSharpKata/StringCalculator.fs
+++ FSharpKata/StringCalculator.fs
@@ -1,1 +1,28 @@
 ﻿module StringCalculator
+
+open System
+open System.Linq
+
+let getValuesAndSeparator (number : string)=
+    match number with
+    | n when n.StartsWith("//") ->
+       let delimiter = [|number.ElementAt(2)|]
+       let number1 = number.Substring(3)
+       number1, delimiter
+    | _ ->
+       let delimiter = [|',';'\n'|]
+       number, delimiter
+
+let getValues (number : string) = 
+    let values, separator = getValuesAndSeparator number
+    values.Split(separator).Select(fun f -> Int32.Parse(f))
+
+let add number =
+    match number with
+    | n when String.IsNullOrEmpty(n) -> Some(0)
+    | _ ->  
+            let parsedInts = getValues number
+            let negativeInts = parsedInts.Where(fun f -> f < 0)
+            match negativeInts with
+            | i when i.Any() -> None
+            | _ -> Some(parsedInts.Sum())
--- FSharpKataTests/StringCalculatorTests.fs
+++ FSharpKataTests/StringCalculatorTests.fs
@@ -6,6 +6,48 @@
 // AddShouldReturnAdditionOfNumbersIfNewLineDelimiterIsPassed
 // AddShouldReturnAdditionOfNumbersIfMultipleNumbersArePassed
 // AddShouldReturnAdditionOfNumbersIfCustomDelimitersArePassed
-// AddShouldThrowIfNegative NumbersArePassed
+// AddShouldThrowIfNegativeNumbersArePassed
 
 open StringCalculator
+open Xunit
+open FsUnit.Xunit
+open Xunit.Extensions
+
+[<Fact>]
+let AddShouldReturn0OnEmptyNumber() =
+    add "" |> should equal (Some(0))
+
+[<Theory>]
+[<InlineData("0", 0)>]
+[<InlineData("1", 1)>]
+[<InlineData("2", 2)>]
+let AddShouldReturnNumberIfSingleNumberIsPassed number expected =
+    add number |> should equal (Some(expected))
+
+[<Theory>]
+[<InlineData("1,2", 3)>]
+[<InlineData("1,3", 4)>]
+[<InlineData("1,2,3", 6)>]
+[<InlineData("1,2,3,4", 10)>]
+let AddShouldReturnAdditionOfNumbersIfManyNumbersArePassed numbers expected =
+    add numbers |> should equal (Some(expected))
+    
+[<Theory>]
+[<InlineData("1\n2", 3)>]
+[<InlineData("1\n3", 4)>]
+[<InlineData("1\n2\n3", 6)>]
+[<InlineData("1\n2\n3,4", 10)>]
+let AddShouldReturnAdditionOfNumbersIfNewLineDelimiterIsPassed numbers expected =
+    add numbers |> should equal (Some(expected))
+
+[<Theory>]
+[<InlineData("//;\n1;2", 3)>]
+[<InlineData("//;\n1;3", 4)>]
+[<InlineData("//-\n1-2-3", 6)>]
+[<InlineData("//-\n1-2-3-4", 10)>]
+let AddShouldReturnAdditionOfNumbersIfCustomDelimitersArePassed numbers expected =
+    add numbers |> should equal (Some(expected))
+
+[<Fact>]
+let AddShouldReturnNoneIfNegativeNumbersArePassed() =
+    add "1,-2" |> should equal None
