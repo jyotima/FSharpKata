19914352ea3f0f7872e5b542bc68cf5c9ab81154
jyotima <jyotima@live.com>
2014-07-25 16:34:10 +0530

    ROP- step 4 - using Option in pipeline

--- FSharpKata/Fizzbuzz.fs
+++ FSharpKata/Fizzbuzz.fs
@@ -2,28 +2,34 @@
 
 // http://fsharpforfunandprofit.com/posts/railway-oriented-programming-carbonated/
 
-type Data = {i:int; label:string option}
-
 let carbonate factor label data =
-    let {i = i; label = labelSoFar} = data
+    let (i, labelSoFar) = data
     if i % factor = 0 then 
         let newLabel =
-            match labelSoFar with
-            | Some s -> s + label
-            | None -> label
-        {data with label = Some newLabel}
+            labelSoFar
+            |> Option.map (fun s -> s + label)
+            |> defaultArg <| label
+        (i, Some newLabel)
     else
         data
 
 let labelOrDefault data = 
-    let { i = i; label = labelSoFar} = data
-    match labelSoFar with
-    | Some s -> s
-    | None -> i.ToString()
+    let (i : int, labelSoFar) = data
+    labelSoFar
+    |> defaultArg <| i.ToString()
+    
+
+let FizzBuzzWithRules rules number =
+    let allRules =
+        rules
+        |> List.map( fun (factor, label) -> carbonate factor label)
+        |> List.reduce(>>)
+
+    (number, None)
+    |> allRules
+    |> labelOrDefault
 
 let Fizzbuzz number = 
-    // FizzBuzzWithRules number
-    {i = number; label = None}
-    |> carbonate 3 "Fizz"
-    |> carbonate 5 "Buzz"
-    |> labelOrDefault
+    let rules = [ (3,"Fizz"); (5,"Buzz"); ]
+    FizzBuzzWithRules rules number
+    
